2a81f8659ba9ad28dbf1a88aade69936de7a7999
gangbo <daigangbo@gmail.com>
2012-07-19 18:05:45 +0800

    weibo login

---
+++ script/config.php
@@ -0,0 +1,4 @@
+<?php
+define( "WB_AKEY" , '2975997035' );
+define( "WB_SKEY" , '226690382bfd9c4ad77b0f9f7dfe8297' );
+define( "CANVAS_PAGE" , "http://apps.weibo.com/woaiping" );
--- script/index.php
+++ script/index.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+header('Content-Type: text/html; charset=UTF-8');
+include('config.php');
+include('saetv2.ex.class.php');
+$json = array();
 $img_arr = array(
     'part-0-0.jpg',
     'part-0-1.jpg',
@@ -12,6 +17,27 @@ $img_arr = array(
 $img_arr = array_map(function($v){
             return "picture/$v";
         },$img_arr);
+//从POST过来的signed_request中提取oauth2信息
+function is_auth(){
+    if(!empty($_REQUEST["signed_request"])){
+        $o = new SaeTOAuthV2( WB_AKEY , WB_SKEY  );
+        $data=$o->parseSignedRequest($_REQUEST["signed_request"]);
+        if($data=='-2'){
+             die('签名错误!');
+        }else{
+            $_SESSION['oauth2']=$data;
+        }
+    }
+    var_dump($_SESSION);
+    //判断用户是否授权
+    if (empty($_SESSION['oauth2']["user_id"])) {//若没有获取到access token，则发起授权请求
+        include "auth.php";
+        exit;
+    } else {//若已获取到access token，则加载应用信息
+        $c = new SaeTClientV2( WB_AKEY , WB_SKEY ,$_SESSION['oauth2']['oauth_token'] ,'' );
+    }
+}
+$json['is_auth'] = is_auth();
 //shuffle($img_arr);
 array_push($img_arr,'xxx.gif');
 include ('aiping.html');
