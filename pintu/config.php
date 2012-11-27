d56755f9c6caa43a2f74fae87c281300a350e220
gangbo.dai <daigangbo@gmail.com>
2012-11-27 10:35:03 +0800

    nothing

--- pintu/config.php
+++ pintu/config.php
@@ -2,4 +2,4 @@
 define( "WB_AKEY" , '2975997035' );
 define( "WB_SKEY" , '226690382bfd9c4ad77b0f9f7dfe8297' );
 define( "CANVAS_PAGE" , "http://apps.weibo.com/woaiping" );
-//define( "CANVAS_PAGE" , "http://game.daigangbo.cn/pintu" );
+//define( "CANVAS_PAGE" , "http://game.daigangbo.cn/pintu/" );
